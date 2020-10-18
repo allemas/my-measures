88da404091ff2d7ae96cc3d697ba73fb67e97c41
Allemand Sébastien <sebastienallemand.as@gmail.com>
2020-10-18 23:30:18 +0200

    refactore balance sheet

--- src/components/BalanceSheet/BalanceSheet.js
+++ src/components/BalanceSheet/BalanceSheet.js
@@ -1,141 +1,143 @@
-import React, {useEffect} from 'react';
-import {Form, Row, Col, Container} from 'react-bootstrap';
-import {useForm, Controller} from "react-hook-form";
-import {TextField} from "@material-ui/core";
-import {Button} from 'react-bootstrap';
+import React, {useEffect, useReducer, useState} from 'react';
+import {Row, Col, Container, Modal, Form, Button} from 'react-bootstrap';
 import {pushbalance, getBalance} from '../../api/weight';
 import DataTable from "react-data-table-component";
-import {connect} from 'react-redux'
 import {BalanceSheetColumn} from "./BalanceSheetColumns";
+import balanceSheetReducer, {initialState} from "../../store/balanceSheet";
+import {useSelector} from 'react-redux'
+import {Fab} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import bodyparts from "../../api/bodyparts";
+
 
 const BalanceSheet = (props) => {
+  const [modalshow, setShow] = useState(false);
+  const user = useSelector(state => state.user);
+  const [state, dispatch] = useReducer(balanceSheetReducer, [], initialState);
 
   useEffect(() => {
-    getBalance({user_uid: '7b129eb6-106a-4327-976d-a9a28e941fa9'}).then(response => {
-      props.loadBalanceSheet(response.data);
+    getBalance({user_uid: user.uid}).then(response => {
+      console.log(response.data);
+      dispatch({
+        type: "BALANCE_SHEET_LOAD_ASYNC",
+        data: response.data
+      });
     }).catch(console.log);
-  });
+  }, [user]);
 
-
-  const {errors, register, control, handleSubmit} = useForm({
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    defaultValues: {},
-    resolver: undefined,
-    context: undefined,
-    criteriaMode: "firstError",
-    shouldFocusError: true,
-    shouldUnregister: true,
-  });
 
   /*
   * https://thoughtbot.com/blog/using-redux-with-react-hooks
-  * */
-  const onSubmit = data => {
+  */
+  const onSubmit = event => {
+    event.preventDefault();
+    const {chest, shoulders, thigh, arms, back, waist} = event.target;
     const date = new Date();
+
+
     pushbalance({
       "date": date.toISOString(),
-      "chest": parseInt(data.chest),
-      "shoulders": parseInt(data.shoulders),
-      "arms": parseInt(data.arms),
-      "back": parseInt(data.back),
-      "waist": parseInt(data.waist),
-      "thigh": parseInt(data.thigh),
-      user: props.user.uid
+      "chest": parseInt(chest.value),
+      "shoulders": parseInt(shoulders.value),
+      "arms": parseInt(arms.value),
+      "back": parseInt(back.value),
+      "waist": parseInt(waist.value),
+      "thigh": parseInt(thigh.value),
+      user: user.uid
     }).then((response) => {
-        let data = response.data;
-        props.publishBalanceSheet({
-            "chest": parseInt(data.chest),
-            "shoulders": parseInt(data.shoulders),
-            "arms": parseInt(data.arms),
-            "back": parseInt(data.back),
-            "waist": parseInt(data.waist),
-            "thigh": parseInt(data.thigh),
-          }
-        );
-      }
-    ).catch(data => console.log(data)
-    )
-    ;
+      dispatch({
+        type: "BALANCE_SHEET_ADD",
+        data: {
+          "date": response.data.date,
+          "chest": response.data.chest,
+          "shoulders": response.data.shoulders,
+          "arms": response.data.arms,
+          "back": response.data.back,
+          "waist": response.data.waist,
+          "thigh": response.data.thigh,
+          user: response.data.user,
+        }
+      });
+    }).catch(console.log);
+
+
   };
 
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div style={{marginTop: 10, textAlign: 'right'}}>
+              <Fab color="primary" variant="extended" style={{boxShadow: "none", background: "#30343F"}}
+                   onClick={() => setShow(true)}>
+                <AddIcon/>
+                Créer un bilan
+              </Fab>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTable
+              title="Evolution"
+              columns={BalanceSheetColumn}
+              responsive={true}
+              data={state.map(item => {
+                var mydate = new Date(item.date);
+                console.log(mydate);
 
-
-  return (<>
-    <Container fluid>
-      <Row>
-        <Col> &nbsp;
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          {errors.chest && errors.chest.message}
-          {errors.shoulders && errors.shoulders.message}
-          {errors.thighs && errors.thighs.message}
-          {errors.arms && errors.arms.message}
-          {errors.back && errors.back.message}
-          {errors.waist && errors.waist.message}
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Row>
-              <Controller as={TextField} name="chest" placeholder="Pectoraux" control={control} defaultValue=""
-                          rules={{required: "Mesure pectoraux requis"}}
-              />
-              <Controller as={TextField} name="shoulders" placeholder="Epaules" control={control} defaultValue=""
-                          rules={{required: "Mesure Epaules requis"}}
-              />
-              <Controller as={TextField} name="thigh" placeholder="Cuisses" control={control} defaultValue=""
-                          rules={{required: "Mesure Cuisses requis"}}
-              />
-              <Controller as={TextField} name="arms" placeholder="Bras" control={control} defaultValue=""
-                          rules={{required: "Mesure Bras requis"}}
-              />
-              <Controller as={TextField} name="back" placeholder="Dos" control={control} defaultValue=""
-                          rules={{required: "Mesure Dos requis"}}
-              />
-              <Controller as={TextField} name="waist" placeholder="Taille" control={control} defaultValue=""
-                          rules={{required: "Mesure Taille requis"}}
-              />
-              <Button type="submit">
-                Envoyer
-              </Button>
-            </Form.Row>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <DataTable
-            title="Evolution"
-            columns={BalanceSheetColumn}
-            responsive={true}
-            data={props.balance.map(item => {
-              var mydate = new Date(item.date);
-              return {
-                ...item,
-                'date': mydate.toLocaleDateString('fr-FR') + ' ' + mydate.toLocaleTimeString('fr-FR')
+                return {
+                  ...item,
+                  'date': mydate.toLocaleDateString('fr-FR') + ' ' + mydate.toLocaleTimeString('fr-FR')
+                }
+              })
               }
-            })
-            }
-            pagination={true}
-          />
-        </Col>
-      </Row>
-    </Container>
-  </>)
+              pagination={true}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <div>
+        <Modal
+          size="xl"
+          show={modalshow}
+          onHide={() => setShow(false)}
+          dialogClassName="modal-90w">
+          <Modal.Header closeButton>
+            <Modal.Title id="example-custom-modal-styling-title">
+              Nouvelle mesure de poids
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row>
+                <Col>&nbsp;</Col>
+                <Col>
+                  <Form onSubmit={onSubmit}>
+                    {
+                      bodyparts.map((item) => {
+                        return (<Form.Group controlId="">
+                            <Form.Control type="text" placeholder={item.label} name={item.selector}/>
+                          </Form.Group>
+                        );
+                      })
+                    }
+
+                    <Button variant="primary" type="submit">
+                      Enregistrer
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+        </Modal>
+      </div>
+    </>
+  )
     ;
-
-
 }
 
+export default BalanceSheet;
 
-const mapStateToProps = (state) => ({
-  user: state.user,
-  balance: state.balanceSheet
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  publishBalanceSheet: data => dispatch({type: 'BALANCE_SHEET_ADD', data: data}),
-  loadBalanceSheet: data => dispatch({type: 'BALANCE_SHEET_LOAD_ASYNC', data: data}),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(BalanceSheet);
--- src/store/balanceSheet.js
+++ src/store/balanceSheet.js
@@ -1,25 +1,26 @@
 import {BALANCE_SHEET_ADD, BALANCE_SHEET_LOAD_ASYNC} from "../actions/ActionTypes";
 
+export const initialState = () => {
+  return []
+}
 const balanceSheetReducer = (state = [], action) => {
   switch (action.type) {
     case BALANCE_SHEET_ADD:
       return [
         ...state,
-
         {
+          "date": action.data.date,
           "chest": parseInt(action.data.chest),
           "shoulders": parseInt(action.data.shoulders),
           "arms": parseInt(action.data.arms),
           "back": parseInt(action.data.back),
           "waist": parseInt(action.data.waist),
           "thigh": parseInt(action.data.thigh),
-
         },
       ]
 
     case BALANCE_SHEET_LOAD_ASYNC:
       return [
-        ...state,
         ...action.data
       ];
 
